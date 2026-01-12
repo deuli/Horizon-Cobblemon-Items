8ac8d0626800e0a41918ea3bf2b00db22c8b38b2
deuli <dacostaluca@hotmail.com>
2026-01-12 01:09:50 +0100

    Implement golden bottle cap item

---
+++ src/main/java/deuli/newdawn/horizoncobblemonitems/item/BottleCapItem.java
@@ -0,0 +1,73 @@
+package deuli.newdawn.horizoncobblemonitems.item;
+
+import com.cobblemon.mod.common.CobblemonSounds;
+import com.cobblemon.mod.common.api.item.PokemonSelectingItem;
+import com.cobblemon.mod.common.api.pokemon.stats.Stat;
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
+import com.cobblemon.mod.common.item.CobblemonItem;
+import com.cobblemon.mod.common.item.battle.BagItem;
+import com.cobblemon.mod.common.pokemon.Pokemon;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
+
+public class BottleCapItem extends CobblemonItem implements PokemonSelectingItem {
+    private final int ivSetAmount;
+    private final Set<Stat> targetStats;
+
+    public BottleCapItem(int ivSetAmount, Set<Stat> targetStats) {
+        super(new Item.Properties());
+        this.ivSetAmount = ivSetAmount;
+        this.targetStats = targetStats;
+    }
+
+    @Override
+    public @Nullable BagItem getBagItem() {
+        return null;
+    }
+
+    private boolean canChangeIV(@NotNull Pokemon pokemon, Stat stat) {
+        return pokemon.getIvs().getEffectiveBattleIV(stat) != ivSetAmount;
+    }
+
+    @Override
+    public boolean canUseOnPokemon(@NotNull ItemStack stack, @NotNull Pokemon pokemon) {
+        return targetStats.stream().anyMatch(stat -> canChangeIV(pokemon, stat));
+    }
+
+    @Override
+    public @Nullable InteractionResultHolder<ItemStack> applyToPokemon(@NotNull ServerPlayer serverPlayer, @NotNull ItemStack itemStack, @NotNull Pokemon pokemon) {
+        if (!canUseOnPokemon(itemStack, pokemon)) {
+            return InteractionResultHolder.fail(itemStack);
+        }
+
+        targetStats.forEach(stat -> {
+            if (canChangeIV(pokemon, stat)) {
+                pokemon.hyperTrainIV(stat, ivSetAmount);
+            }
+        });
+
+        itemStack.consume(1, serverPlayer);
+        PokemonEntity entity = pokemon.getEntity();
+        if (entity != null)
+            entity.playSound(CobblemonSounds.MEDICINE_PILLS_USE, 1F, 1F);
+
+        return InteractionResultHolder.success(itemStack);
+    }
+
+    @Override
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level world, @NotNull Player player, @NotNull InteractionHand hand) {
+        if (player instanceof ServerPlayer serverPlayer)
+            return use(serverPlayer, player.getItemInHand(hand));
+
+        return InteractionResultHolder.success(player.getItemInHand(hand));
+    }
+}
